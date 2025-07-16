f0244e042dc1a65c70209b5104115bbed7988fcd
anchit15233 <152762797+anchit15233@users.noreply.github.com>
2025-07-16 13:50:44 +0530

    main.py

--- main.py
+++ main.py
@@ -33,5 +33,9 @@ def analyze_resume():
         print("❌ Error in analyze_resume():", e)
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
